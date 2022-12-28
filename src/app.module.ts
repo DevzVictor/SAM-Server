6e2c38bb22e0bcc407da8c13716c28dfde6051f0
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2022-12-28 11:39:07 -0300

    feat(patient): add nest arqui crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PatientModule } from './patient/patient.module';
 
 @Module({
-  imports: [],
+  imports: [PatientModule],
   controllers: [AppController],
   providers: [AppService],
 })
