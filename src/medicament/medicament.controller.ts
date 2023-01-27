2516fc24e1891cc9b12d039e8c5cd316c2308c07
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-01-27 00:25:06 -0300

    feat(medicament): create all endpoints in crontroller

--- src/medicament/medicament.controller.ts
+++ src/medicament/medicament.controller.ts
@@ -1,34 +1,64 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
 import { MedicamentService } from './medicament.service';
 import { CreateMedicamentDto } from './dto/create-medicament.dto';
 import { UpdateMedicamentDto } from './dto/update-medicament.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('medicament')
 @Controller('medicament')
 export class MedicamentController {
   constructor(private readonly medicamentService: MedicamentService) {}
 
   @Post()
+  @ApiOperation({
+    summary: 'Criar controle de medicação',
+  })
   create(@Body() createMedicamentDto: CreateMedicamentDto) {
     return this.medicamentService.create(createMedicamentDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Listar todos os controles de medicação',
+  })
   findAll() {
     return this.medicamentService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: 'Mostrar um controle de medicação pelo ID',
+  })
   findOne(@Param('id') id: string) {
-    return this.medicamentService.findOne(+id);
+    return this.medicamentService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateMedicamentDto: UpdateMedicamentDto) {
-    return this.medicamentService.update(+id, updateMedicamentDto);
+  @ApiOperation({
+    summary: 'Editar um controle de medicação pelo ID',
+  })
+  update(
+    @Param('id') id: string,
+    @Body() updateMedicamentDto: UpdateMedicamentDto,
+  ) {
+    return this.medicamentService.update(id, updateMedicamentDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.medicamentService.remove(+id);
+  @ApiOperation({
+    summary: 'Deletar um controle de medicação pelo ID',
+  })
+  delete(@Param('id') id: string) {
+    return this.medicamentService.delete(id);
   }
 }
