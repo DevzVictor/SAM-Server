41e6e2888c3f7d99b364e07530ac216d050875f2
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-01-27 00:09:38 -0300

    feat(medicament): create 'dto'

--- src/medicament/dto/create-medicament.dto.ts
+++ src/medicament/dto/create-medicament.dto.ts
@@ -1,1 +1,34 @@
-export class CreateMedicamentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsPositive, IsString } from 'class-validator';
+
+export class CreateMedicamentDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome do medicamento',
+    example: 'Dipirona',
+  })
+  name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Quantidade do medicamento',
+    example: '20 gotas',
+  })
+  quantity: string;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Intervalo de horas para cada dose do medicamento',
+    example: 2,
+  })
+  repeatHour: number;
+
+  @IsNumber()
+  @IsPositive()
+  @ApiProperty({
+    description: 'Intervalo de minutos para cada dose do medicamento',
+    example: 30,
+  })
+  repeatMinutes: number;
+}
