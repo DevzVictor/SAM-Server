053321d518a9e161f1d092ff7d91deb9b398e11d
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-01-27 00:32:30 -0300

    feat(medicament): imports 'Prisma service' in Module

--- src/medicament/medicament.module.ts
+++ src/medicament/medicament.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MedicamentService } from './medicament.service';
 import { MedicamentController } from './medicament.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [PrismaService],
   controllers: [MedicamentController],
-  providers: [MedicamentService]
+  providers: [MedicamentService],
 })
 export class MedicamentModule {}
