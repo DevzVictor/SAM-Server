036ba8240bb2b7c360d4d9717d357ae2a4cda0ce
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-01-26 22:54:09 -0300

    feat(exam): imports 'prismaModule' in exam module

--- src/exam/exam.module.ts
+++ src/exam/exam.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ExamService } from './exam.service';
 import { ExamController } from './exam.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ExamController],
   providers: [ExamService],
 })
