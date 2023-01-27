abc0f0ce17972ef9ad6fcfefe8aee268bcf05a6b
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-01-27 00:48:14 -0300

    feat(vaccine): create dto

--- src/vaccine/dto/create-vaccine.dto.ts
+++ src/vaccine/dto/create-vaccine.dto.ts
@@ -1,1 +1,25 @@
-export class CreateVaccineDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class CreateVaccineDto {
+  @IsString()
+  @ApiProperty({
+    description: 'Nome da Vacina',
+    example: 'Vacina COVID-19 primeira dose',
+  })
+  name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Data para primeira dose',
+    example: '18/04/23',
+  })
+  firstDoseDate: string;
+
+  @IsString()
+  @ApiProperty({
+    description: 'Data para segunda dose',
+    example: '20/06/23',
+  })
+  nextDoseDate: string;
+}
