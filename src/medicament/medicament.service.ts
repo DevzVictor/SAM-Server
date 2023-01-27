da0076cf6ef33f30faf75cb0c8687c51087c5eae
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-01-27 00:25:48 -0300

    feat(medicament): implement routes 'create', 'findAll' and findById

--- src/medicament/medicament.service.ts
+++ src/medicament/medicament.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateMedicamentDto } from './dto/create-medicament.dto';
 import { UpdateMedicamentDto } from './dto/update-medicament.dto';
+import { Medicament } from './entities/medicament.entity';
+import { randomUUID } from 'crypto';
+import { handleError } from 'src/utils/handle-error.util';
 
 @Injectable()
 export class MedicamentService {
-  create(createMedicamentDto: CreateMedicamentDto) {
-    return 'This action adds a new medicament';
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(createMedicamentDto: CreateMedicamentDto): Promise<Medicament> {
+    const medicament: Medicament = { ...createMedicamentDto, id: randomUUID() };
+
+    return this.prisma.medicament
+      .create({
+        data: medicament,
+      })
+      .catch(handleError);
   }
 
-  findAll() {
-    return `This action returns all medicament`;
+  findAll(): Promise<Medicament[]> {
+    return this.prisma.medicament.findMany();
   }
 
-  findOne(id: number) {
+  async findById(id: string): Promise<Medicament> {
+    const record = await this.prisma.medicament.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    if (!record) {
+      throw new NotFoundException(`Registro com o '${id}' não encontrado.`);
+    }
+
+    return record;
+  }
+
+  findOne(id: string) {
     return `This action returns a #${id} medicament`;
   }
 
-  update(id: number, updateMedicamentDto: UpdateMedicamentDto) {
+  update(id: string, updateMedicamentDto: UpdateMedicamentDto) {
     return `This action updates a #${id} medicament`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} medicament`;
   }
 }
