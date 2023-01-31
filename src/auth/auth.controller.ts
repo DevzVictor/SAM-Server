f9a3173998b9b8b1e4c16459e2fc0af45d08a19a
Victor Rodrigues <91481122+DevzVictor@users.noreply.github.com>
2023-01-31 15:03:02 -0300

    feat(auth): implements all routes in controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,41 +3,38 @@ import {
   Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
+  HttpCode,
+  HttpStatus,
+  UseGuards,
 } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { loginResponseDto } from './dto/login-response.dto';
 import { LoginDto } from './dto/login.dto';
 import { LoggedUser } from './logged-user.decorator';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({
+    summary: 'Realizar login usando token de autentificação',
+  })
+  login(@Body() loginDto: LoginDto): Promise<loginResponseDto> {
+    return this.authService.login(loginDto);
   }
 
   @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @UseGuards(AuthGuard())
+  @ApiOperation({
+    summary: 'Retorna usuário autenticado no momento',
+  })
+  @ApiBearerAuth()
+  profile(@LoggedUser() user: User) {
+    return user;
   }
 }
